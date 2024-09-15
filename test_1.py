b6497d87cbee487f680d985fc32393f0d13d7e06
aishwarya <swamiaishwarya24@gmail.com>
2024-09-15 23:18:23 +0530

    builtin markers

--- test_1.py
+++ test_1.py
@@ -8,3 +8,27 @@ def test_smoke():
 @pytest.mark.regression
 def test_regression():
     print('regression test cases passed')
+
+@pytest.mark.skip
+def test_skip():
+    print('skip test cases')
+
+
+@pytest.mark.skipif(reason='Test case is not yet ready')
+def test_skipif():
+    print('skipif test cases ')
+
+@pytest.mark.parametrize('username,password',
+                         [
+                             ('Aishwarya','Aish123'),
+                             ('as460','pwd')
+                         ]
+                         )
+def test_parameterize(username,password):
+    print('parameterize test cases passed')
+
+
+@pytest.mark.xfail
+def test_xfail():
+    assert True
+    print('xfail test cases')
